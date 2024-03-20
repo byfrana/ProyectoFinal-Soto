5b24058c01035711d55183ac1e4941bd611290db
Fran <fransotoprado@gmail.com>
2024-03-19 22:33:53 -0300

    json y correcciones

--- js/gallery.js
+++ js/gallery.js
@@ -1,33 +1,49 @@
 //carrito de compras
-
-const productos = [
-	{ codigo: 1, nombre: 'Jason Wu', precio: 35000, img: '01-Jason-wu.jpg' },
-	{ codigo: 2, nombre: 'Altuzarra', precio: 28000, img: '02-Altuzarra.jpg' },
-	{ codigo: 3, nombre: 'Thakon', precio: 32000, img: '03-Thakoon.jpg' },
-	{ codigo: 4, nombre: 'Dolce & Gabbana', precio: 32000, img: '04-DolceGabbana.jpg' },
-	{ codigo: 5, nombre: 'Osman', precio: 33000, img: '05-Osman.jpg' },
-	{ codigo: 6, nombre: 'Tagliapietra', precio: 28000, img: '06-Tagliapietra.jpg' },
-	{ codigo: 7, nombre: 'Versace', precio: 30000, img: '07-Versace.jpg' },
-	{ codigo: 8, nombre: 'Chanel', precio: 26000, img: '08-Chanel.jpg' },
-	{ codigo: 9, nombre: 'Burberry', precio: 40000, img: '10-Burberry.jpg' },
-];
-
 let elementosCarrito = [];
+let productos = [];
+
 const contenedorProductos = document.getElementById('grid');
 const contenedorElementosCarrito = document.getElementById('carrito');
 const totalSpan = document.getElementById('total');
 
-function renderizarProductos() {
-	productos.forEach((producto) => {
-		const card = document.createElement('card');
-		card.innerHTML = `
-      <img src=./img/${producto.img}>
-			<h3>${producto.nombre}</h3>
-      <p>CLP $${producto.precio}</p>
-      <button class="btn-agregar-carrito" data-id="${producto.codigo}">Agregar</button>`;
-		contenedorProductos.appendChild(card);
-	});
+async function obtenerProductos() {
+	try {
+		const response = await fetch('./productos.json');
+		if (!response.ok) {
+			throw new Error('No se pudo cargar el archivo de productos.');
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error al cargar los productos:', error);
+		return [];
+	}
 }
+
+async function renderizarProductos() {
+	try {
+		// Obtiene los productos usando la función obtenerProductos
+		productos = await obtenerProductos();
+		// Limpia el contenedor de productos antes de renderizar los nuevos productos
+		contenedorProductos.innerHTML = '';
+		// Renderiza los productos obtenidos
+		productos.forEach((producto) => {
+			const card = document.createElement('div');
+			card.classList.add('card');
+			card.innerHTML = `
+							<img src="./img/${producto.img}">
+							<h3>${producto.nombre}</h3>
+							<p>CLP $${producto.precio}</p>
+							<button class="btn-agregar-carrito" data-id="${producto.codigo}">Agregar</button>`;
+			contenedorProductos.appendChild(card);
+		});
+	} catch (error) {
+		console.error('Error al renderizar los productos:', error);
+	}
+}
+
+// Llama a la función para renderizar los productos
+renderizarProductos();
 
 //agregar al carrito el producto
 
@@ -91,7 +107,16 @@ function realizarCompra() {
 	// const precioNeto = precioTotal / 1.19;
 	// const precioIVA = precioTotal - precioNeto;
 
-	alert(`Compra finalizada\nTotal: $${precioTotal.toFixed(0)}`);
+	// alert(`Compra finalizada\nTotal: $${precioTotal.toFixed(0)}`);
+	Toastify({
+		text: `Compra finalizada\nTotal: $${precioTotal.toFixed(0)}`,
+		duration: 5000, // Duración de la notificación en milisegundos (opcional)
+		gravity: 'top', // Posición de la notificación (opcional)
+		position: 'center', // Posición de la notificación (opcional)
+		backgroundColor: 'linear-gradient(to right, brown, #2e1414)', // Color de fondo (opcional)
+		stopOnFocus: true, // Detener la notificación al hacer clic en ella (opcional)
+	}).showToast();
+
 	elementosCarrito.length = 0;
 	renderizarCarrito();
 }
